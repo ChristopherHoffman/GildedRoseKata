4f711a0431710d4e8c4df6c1a08da761e2468e1c
Christopher Hoffman <CSH.Hoffman@gmail.com>
2016-02-20 20:26:41 -0500

    Add Sulfuras and BackstagePass Tests

---
+++ GildedRose.ConsoleTests/BackStagePassesTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GildedRose.Console;
+
+
+namespace GildedRose.ConsoleTests
+{
+    [TestClass]
+    public class BackStagePassesTests
+    {
+        public Program program;
+        int originalQuality = 80;
+        int originalSellIn = 0;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            program = new Program();
+        }
+
+        public Item getPass(int sellIn, int quality)
+        {
+            return new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                SellIn = sellIn,
+                Quality = quality
+            };
+        }
+
+        [TestMethod]
+        public void BackstagePass_QualityIncreaseByOne_SellInDateGreaterThanTen()
+        {
+            int originalQuality = 20;
+            int originalSellIn = 15;
+            program.Items = new List<Item> { getPass(originalSellIn, originalQuality) };
+            program.UpdateQuality();
+
+            int newQuality = program.Items[0].Quality;
+
+            Assert.AreEqual(originalQuality+1, newQuality);
+        }
+
+        [TestMethod]
+        public void BackstagePass_QualityIncreaseByTwo_SellInDateBetweenTenAndSix()
+        {
+
+            int originalQuality = 20;
+            int originalSellIn = 10;
+            program.Items = new List<Item> { getPass(originalSellIn, originalQuality) };
+            program.UpdateQuality();
+
+            int newQuality = program.Items[0].Quality;
+
+            Assert.AreEqual(originalQuality + 2, newQuality);
+        }
+
+        [TestMethod]
+        public void BackstagePass_QualityIncreaseByTwo_SellInDateBetweenFiveAndZero()
+        {
+
+            int originalQuality = 20;
+            int originalSellIn = 5;
+            program.Items = new List<Item> { getPass(originalSellIn, originalQuality) };
+            program.UpdateQuality();
+
+            int newQuality = program.Items[0].Quality;
+
+            Assert.AreEqual(originalQuality + 3, newQuality);
+        }
+
+        [TestMethod]
+        public void BackstagePass_QualityShouldBeZero_WhenSellInDateNegative()
+        {
+
+            int originalQuality = 20;
+            int originalSellIn = 0;
+            program.Items = new List<Item> { getPass(originalSellIn, originalQuality) };
+            program.UpdateQuality();
+
+            int newQuality = program.Items[0].Quality;
+
+            Assert.AreEqual(0, newQuality);
+        }
+
+        [TestMethod]
+        public void BackstagePass_QualityCannotBeMoreThanFifty()
+        {
+            int originalQuality = 49;
+            int originalSellIn = 10;
+            program.Items = new List<Item> { getPass(originalSellIn, originalQuality) };
+            program.UpdateQuality();
+
+            int newQuality = program.Items[0].Quality;
+
+            Assert.AreEqual(50, newQuality);
+
+            program.UpdateQuality();
+            newQuality = program.Items[0].Quality;
+
+            Assert.AreEqual(50, newQuality);
+        }
+    }
+}
---
+++ GildedRose.ConsoleTests/SulfurasTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GildedRose.Console;
+
+
+namespace GildedRose.ConsoleTests
+{
+    [TestClass]
+    public class SulfurasTests
+    {
+        public Program program;
+        int originalQuality = 80;
+        int originalSellIn = 0;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            program = new Program();
+            program.Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = originalSellIn, Quality = originalQuality } };
+        }
+
+        [TestMethod]
+        public void Sulfuras_NotDecreaseInQuality()
+        {
+            program.UpdateQuality();
+
+            int newQuality = program.Items[0].Quality;
+
+            Assert.AreEqual(originalQuality, newQuality);
+        }
+
+        [TestMethod]
+        public void Sulfuras_SellInShouldNotDecrease()
+        {
+            program.UpdateQuality();
+
+            int newSellIn = program.Items[0].SellIn;
+
+            Assert.AreEqual(originalSellIn, newSellIn);
+        }
+    }
+}
